850d96bfab704ac3f9b6c4e3a8677d85a2ee0789
Stepan <sud_pravo@bk.ru>
2023-04-20 21:27:33 +0700

    первая задача решена

---
+++ task.py
@@ -0,0 +1,10 @@
+from random import randint
+
+a = int (input("Ввведите длинну массива "))
+b = int (input("Ввведите число которое надонайти "))
+array = []
+count = 0
+for i in range(a):
+    array.append(randint(0,9))
+print(array)
+print("Число " , b,"содержится в массиве" , array.count(b), "раз")
